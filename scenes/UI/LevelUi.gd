30c1d5218b29a9b9ce6725ae50c4434ac4dd1546
dirksteynberg <dirkscgm@gmail.com>
2021-12-02 09:09:32 +0200

    Added Coin UI

---
+++ scenes/UI/LevelUi.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+func _ready():
+	var base_level = get_tree().get_nodes_in_group("base_level")
+	
+	if base_level.size() > 0:
+		base_level[0].connect("coin_total_change", self, "on_coin_total_change")
+
+
+func on_coin_total_change(total_coins, collected_coins):
+	$MarginContainer/CoinLabel.text = str(collected_coins, "/", total_coins)
